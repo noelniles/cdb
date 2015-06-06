96695670a421a368ebbc20f273f5ba25ea02adb4
Noel Niles <shakabra321@gmail.com>
2015-06-06 13:15:50 -1000

    Fix Junk Left From Merge

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-$debug = true;
-if ($debug)
-    error_reporting(-1);
-
 require 'vendor/autoload.php';
-<<<<<<< HEAD
 
 /* debugging -- remove or set to false in production */
 use shakabra\cdb\dbghelper;
@@ -14,16 +9,3 @@ $helper = new dbgHelper();
 /* parses request and finds the approprate controller */
 use shakabra\cdb\Router;
 $router = new Router();
-=======
-
-use shakabra\cdb\Router;
-
-echo "<br>";
-
-foreach ($_SERVER as $k => $v) {
-    printf("%s %s<br>", $k, $v);
-}
-$router = new Router($_SERVER['REQUEST_URI']);
-?>
->>>>>>> 95916c11d4ebcdfd556e9a3768c5e21cca1e451d
-
--- src/Router.php
+++ src/Router.php
@@ -5,7 +5,6 @@ use shakabra\cdb\BaseController;
 
 class Router
 {
-<<<<<<< HEAD
     private $cntl;
     private $dbname;
     private $request_uri;
@@ -17,48 +16,6 @@ class Router
         $this->cntl = new BaseController($this->dbname);
         if (DEBUG) {
             $this->dump_object();
-=======
-    private $cntl; /* controller */
-    private $reqd; /* requested resource */
-    private $okay_routes = ["home",
-                            "git",
-                            "projects",
-                            "posts",
-                            "cv"
-                           ];
-
-    public function __construct($requested)
-    {
-        echo "<br>Router contruction begining<br>";
-        $this->cntl = new BaseController();
-        $this->reqd = $this->parse_request($requested);
-        $this->find_requested_controller($this->reqd);
-    }
-    
-    public function __destruct()
-    {
-        echo "<br>Destroying the router</br>";
-    } 
-    
-    private function parse_request($request)
-    {
-        $parsed_request = parse_url($request);
-        $parsed_request["params"] = explode("&", $parsed_request["query"]);
-    }
-    
-    /* @param request string
-     * @return void
-     */
-    private function find_requested_controller($requested)
-    {
-        if (in_array($requested, $this->okay_routes)) {
-            /* route $r to appropriate controller */
-            echo "<br>sweet route bro</br>";
-
-        } else {
-            /* report that $r is unroutable */
-            echo "<br>no bueno</br>";
->>>>>>> 95916c11d4ebcdfd556e9a3768c5e21cca1e451d
         }
         $this->cntl->all_docs();
     }
